1dbebe7579c7ec48f726f9e8d5967a812b132ecf
h3dema <henriquemoura@hotmail.com>
2020-02-20 12:16:40 +0100

    first commit

---
+++ grafo.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from copy import deepcopy
+
+# definição das cores dos vértices
+# cores são usadas nas pesquisas
+WHITE = 0
+GREY = 1
+BLACK = 2
+
+
+class grafo():
+
+    def __init__(self):
+        self.V = []
+        self.E = dict()
+        self.dist = dict()
+        self.cor = dict()
+        self.tf = dict()  # usado no DFS
+
+    @property
+    def num_vertices(self):
+        return len(self.V)
+
+    @property
+    def num_arestas(self):
+        return sum(len(self.E[v]) for v in self.V)
+
+    def acresc_vertice(self, v):
+        if v not in self.V:
+            self.V.append(v)
+            self.cor[v] = WHITE
+            self.dist[v] = float('inf')
+
+    def _add(self, u, v):
+        if u not in self.E:
+            self.E[u] = []
+        self.E[u].append(v)
+
+    def acresc_aresta(self, u, v, direcionada=False):
+        self.acresc_vertice(u)
+        self.acresc_vertice(v)
+        self._add(u, v)
+        if not direcionada:
+            self._add(v, u)
+
+    def __str__(self, s=None):
+        _str = ''
+        V1 = deepcopy(self.V)  # V1 keeps track of the visited vertices
+        while len(V1) > 0:
+            if s is None:
+                s = V1[0]
+            V = [s]
+            while len(V) > 0:
+                v = V.pop(0)
+                V1.remove(v)
+                _str += "{}: {}\n".format(v, [u for u in self.E[v]])
+                V.extend([u for u in self.E[v] if u in V1 and u not in V])
+            s = None
+        return _str
+
+
+if __name__ == '__main__':
+    G = grafo()
+    G.acresc_aresta('a', 'b')
+    G.acresc_aresta('a', 's')
+    G.acresc_aresta('s', 'b')
+    G.acresc_aresta('s', 'c')
+    G.acresc_aresta('s', 'e')
+    G.acresc_aresta('f', 'e')
+    G.acresc_aresta('f', 'c')
+    G.acresc_aresta('f', 'd')
+    G.acresc_aresta('e', 'g')
+    G.acresc_aresta('e', 'd')
+    G.acresc_aresta('d', 'g')
+    G.acresc_aresta('d', 'c')
+
+    print("numero de vértices", G.num_vertices)
+    print("numero de arestas", G.num_arestas)
+    print(G)
